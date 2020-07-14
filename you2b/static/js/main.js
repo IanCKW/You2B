285ccb56b0266f8fa8ad8223915614010248a758
IanCKW <55784952+IanCKW@users.noreply.github.com>
2020-07-14 18:00:02 +0800

    Added the pagination and multiprocessing feature

--- you2b/static/js/main.js
+++ you2b/static/js/main.js
@@ -1,110 +1,174 @@
 var removeStack = [];
 
-function need_button(){
-    if (removeStack.length === 0) {
-        document.getElementById("undo").style.display='none';
-    } else {
-        document.getElementById("undo").style.display='unset';
-    }
-}
+$(document).ready(function() {
+  var entry = {
+    message: "message",
+  };
 
-$(document).on('click','button.removeButton',function(){
-    $(this).parent().parent()[0].style.display="none";
-    var videoId = $(this).parent().parent().attr("id");
-    removeStack.push(videoId);
-    need_button();
-	
-	var entry = {
-        message : videoId,
-    };
-	
-    fetch(`${window.origin}/del-vid`,{
-        method:"POST",
-        credentials: "include",
-        body: JSON.stringify(entry),
-        cache: "no-cache",
-        headers: new Headers({
-            "content-type":"application/json"
+  fetch(`${window.origin}/obtain-video-data`, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify(entry),
+      cache: "no-cache",
+      headers: new Headers({
+        "content-type": "application/json"
+      })
+    })
+    .then(function(response) {
+      response.json()
+        .then(function(data) {
+        // setting the state
+          state.monthlyData = data["monthly_data"];
+          state.userDatetime = data["user_datetime"];
+          data["new_user"] ? state.userDatetime = data["month_by_index"][0]: state.userDatetime = data["user_datetime"] ;
+          state.currentYearMonth = state.userDatetime.slice(0, 7);
+          state.numMonths = Object.keys(data["monthly_data"]).length;
+          state.monthByIndex = data["month_by_index"] // key: index, value: month
+          state.indexByMonth = data["index_by_month"]// key: month, value: index
+          state.currentMonthIndex= state.indexByMonth[state.currentYearMonth];
+          paginate();
         })
     })
 });
 
-$(document).on('click','button#undo',function(){
-    if (removeStack.length > 0) {
-		var videoId = removeStack.pop()
-        document.getElementById(videoId).style.display='unset';
-		
-		var entry = {
-			message : videoId,
-		};
-		
-		fetch(`${window.origin}/undel-vid`,{
-			method:"POST",
-			credentials: "include",
-			body: JSON.stringify(entry),
-			cache: "no-cache",
-			headers: new Headers({
-				"content-type":"application/json"
-			})
-		})
+//state contains all variables needed for pagination. It changes based on which page you're on
+var state ={
+    monthlyData: {},
+    userDatetime: "",
+    currentYearMonth: "",
+
+    // for buttons
+    numMonths: 0,
+    monthByIndex:{},
+    indexByMonth:{},
+    currentMonthIndex:0,
+    numButtons: 5,
+
+}
+
+function paginate() {
+    create_buttons();
+    display_month();
+}
+
+function create_buttons() {
+    var buttons = $('.pageButtons');
+    var numButtons = state.numButtons;
+    var currentMonthIndex = state.currentMonthIndex;
+    var numMonths = state.numMonths;
+
+    var maxLeft = (currentMonthIndex - Math.floor(numButtons / 2));
+    var maxRight = (currentMonthIndex +1+  Math.floor(numButtons / 2));
+
+    if (maxLeft < 1) {
+        maxLeft = 0;
+        maxRight = numButtons;
     }
-    need_button();
+    if (maxRight > numMonths) {
+        maxLeft = numMonths - (numButtons - 1);
+        if (maxLeft < 1){
+            maxLeft = 0;
+        }
+        maxRight = numMonths;
+    }
+    for (var i=maxLeft; i<maxRight; i++ ){
+        var yearMonth = state.monthByIndex[i];
+        var buttonDetails ='<button class="pageButton" id='+yearMonth+'>'+ yearMonth +'</button>';
+        buttons.append(buttonDetails);
+    }
+
+}
+
+$(document).on('click', 'button.pageButton', function(){
+    $(".results").empty();
+    $(".pageButtons").empty();
+    var prev = state.currentYearMonth //for video removal
+
+    var yearMonth = this.id;
+    state.currentYearMonth = yearMonth;
+    state.currentMonthIndex = state.indexByMonth[yearMonth];
+
+    // removing all the removed videos from the page the user was at prior to clicking the btn
+    if(removeStack!==[]){
+        var prevPage = state.monthlyData[prev];
+        var result = [];
+        for (var i=0;i<prevPage.length;i++){
+            if(!removeStack.includes(prevPage[i]["id"])){
+                result.push(prevPage[i])
+            }
+        }
+        removeStack = [];
+        state.monthlyData[prev] = result;
+    }
+
+    need_button(); // remove the undo button
+    paginate();
+
 });
 
+function display_month() {
+  monthData = state.monthlyData[state.currentYearMonth];
+  for (var i = 0; i < monthData.length; i++) {
+    var video =
+      '<div id=' + monthData[i]["id"] + ' class="videoDetails"> ' +
+        '<a href=' + monthData[i]["link"] + ' target="_blank"> ' +
+            '<img class="thumb" src=' + monthData[i]["img"] + ' width="160" height="90">' +
+        '</a>' +
+        '<div class="buttonContainer">' +
+            '<button class="removeButton">-</button>' +
+        '</div>' +
+      '</div>';
+    $('.results').append(video);
+  }
+}
 
+function need_button() {
+  if (removeStack.length === 0) {
+    document.getElementById("undo").style.display = 'none';
+  } else {
+    document.getElementById("undo").style.display = 'unset';
+  }
+}
 
+$(document).on('click', 'button.removeButton', function() {
+  $(this).parent().parent()[0].style.display = "none";
+  var videoId = $(this).parent().parent().attr("id");
+  removeStack.push(videoId);
+  need_button();
 
+  var entry = {
+    message: videoId,
+  };
 
+  fetch(`${window.origin}/del-vid`, {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(entry),
+    cache: "no-cache",
+    headers: new Headers({
+      "content-type": "application/json"
+    })
+  })
+});
 
+$(document).on('click', 'button#undo', function() {
+  if (removeStack.length > 0) {
+    var videoId = removeStack.pop()
+    document.getElementById(videoId).style.display = 'unset';
 
+    var entry = {
+      message: videoId,
+    };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//document.getElementById("testButton").addEventListener("click",testFunc);
-//
-//function testFunc(){
-//    var entry = {
-//        message : "hello",
-//    };
-//
-//    fetch(`${window.origin}/create-entry`,{
-//        method:"POST",
-//        credentials: "include",
-//        body: JSON.stringify(entry),
-//        cache: "no-cache",
-//        headers: new Headers({
-//            "content-type":"application/json"
-//        })
-//    })
-//    .then(response=> {
-//        if(response.status !== 200){
-//            console.log("Response status is not 200. It is :" + response.status);
-//            return;
-//        }
-//        response.json()
-//        .then(data=>{
-//            console.log(data);
-//        });
-//    });
-//
-//}
+    fetch(`${window.origin}/undel-vid`, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify(entry),
+      cache: "no-cache",
+      headers: new Headers({
+        "content-type": "application/json"
+      })
+    })
+  }
+  need_button();
+});
--- you2b/watch.py
+++ you2b/watch.py
@@ -1,26 +1,19 @@
 # Standard libraries
-import json
-import os
-
+import asyncio
+import concurrent.futures
+import time
 # OAuth2
 import google.oauth2.credentials
 import googleapiclient.discovery
-
 # Third-party libraries
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for, session, jsonify, make_response
+    Blueprint, render_template, request, session, jsonify, make_response
 )
 from flask_login import (
-    LoginManager,
     current_user,
-    login_required,
-    login_user,
-    logout_user,
 )
-from googleapiclient.discovery import build
 
 # Internal imports
-from you2b.auth import login_required
 from you2b.db import get_db
 from .user import User
 
@@ -34,33 +27,7 @@ bp = Blueprint('watch', __name__)
 @bp.route('/')
 def index():
     if current_user.is_authenticated:
-        # Get credentials from the current session
-        credentials = google.oauth2.credentials.Credentials(**session['credentials'])
-        
-        # Use credentials to build an API client
-        youtube = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
-        
-        # New videos to populate page with
-        new_videos = collect_videos(youtube)
-        
-        # After videos are collected, update the last visited time of the user
-        User.update_time(current_user.id)
-        
-        # Collect old videos from the database
-        db = get_db()       
-        old_videos = db.execute(
-            "SELECT date_time, video_id, video_url, video_img FROM video "
-            "WHERE user_id = ? AND deleted = 0 " 
-            "ORDER BY date_time DESC", (current_user.id,)
-        ).fetchall()
-        for video in new_videos:
-            db.execute(
-                "INSERT INTO video (user_id, video_id, video_url, video_img, date_time, deleted) "
-                "VALUES (?, ?, ?, ?, ?, 0)",
-                (current_user.id, video[1], video[2], video[3], video[0])
-            )
-        db.commit()
-        return render_template('watch/index.html', new_videos=new_videos, old_videos=old_videos)
+        return render_template('watch/index.html')
     else:
         return (
             '<p>You are not logged in to a YouTube account with a channel.</p>'
@@ -74,7 +41,7 @@ def collect_videos(youtube):
             find_subs_request = youtube.subscriptions().list(
                 part = "snippet",
                 mine = True,
-                maxResults = 15,
+                maxResults = 50,
                 order = "alphabetical",
                 pageToken = token
             )
@@ -83,7 +50,7 @@ def collect_videos(youtube):
                 part = "snippet",
                 mine = True,
                 order = "alphabetical",
-                maxResults = 15
+                maxResults = 50
             )
 
         find_subs_response = find_subs_request.execute()
@@ -104,38 +71,98 @@ def collect_videos(youtube):
     subs = collect_video_page(youtube, "", [])
     
     videos = []
-    
-    # For each subscription, find the playlist corresponding to that channel's uploads
-    for cid in subs:
-        chan_uploads_request = youtube.channels().list(
-            part = "contentDetails",
-            id = cid
-        )
-        chan_uploads_response = chan_uploads_request.execute()
-        chan_upload_id = chan_uploads_response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-        
-        # Take latest 30 videos from each channel
-        get_uploaded_vids_request = youtube.playlistItems().list(
-            part = "snippet",
-            playlistId = chan_upload_id,
-            maxResults = 30
-        )
-        get_uploaded_vids_response = get_uploaded_vids_request.execute()
-        for video in get_uploaded_vids_response['items']:
-            dateTime = video['snippet']['publishedAt'] # FORMAT: "0000-00-00T00:00:00Z" eg "2020-05-25T15:00:34Z"
-            
-            # Only add the video if it hasn't already been captured on the last visit
-            if dateTime > current_user.last_visited:
-                thumb = video['snippet']['thumbnails']['medium']
-                img = thumb['url']
-                id = video['snippet']['resourceId']['videoId']
-                link = "https://www.youtube.com/watch?v=" + video['snippet']['resourceId']['videoId']
 
-                videos.append((dateTime, id, link, img))
-    
+    # For each subscription, find the playlist corresponding to that channel's uploads
+
+    subsIter = ((youtube,current_user.last_visited, cid) for cid in subs) #creating a generator for my get_uploads function that uses multiprocessing
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = executor.map(get_uploads_wrapper,subsIter)
+        for result in results:
+            videos += result
+
     # Sort and return list of videos.
-    videos = sorted(videos, reverse=True)    
+    end = time.perf_counter()
+    videos = sorted(videos, reverse=True)
+    print(len(videos))
     return videos
+
+#wrapper required to pass in multiple parameters into the map function. Can't use lambda
+#due to pickling (a compromise of multiprocessing to overcome GIL issues. One condition
+# of pickling is that the function must be top level)
+def get_uploads_wrapper(p):
+    return get_uploads(*p)
+
+#returns an array of all the videos from one channel
+def get_uploads(youtube,last_visited, cid):
+    videos_of_this_channel = []
+    chan_uploads_request = youtube.channels().list(
+        part="contentDetails",
+        id=cid
+    )
+    chan_uploads_response = chan_uploads_request.execute()
+    chan_upload_id = chan_uploads_response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+    # Take latest 30 videos from each channel
+    get_uploaded_vids_request = youtube.playlistItems().list(
+        part="snippet",
+        playlistId=chan_upload_id,
+        maxResults=50
+    )
+    get_uploaded_vids_response = get_uploaded_vids_request.execute()
+    for video in get_uploaded_vids_response['items']:
+        dateTime = video['snippet']['publishedAt']  # FORMAT: "0000-00-00T00:00:00Z" eg "2020-05-25T15:00:34Z"
+        # Only add the video if it hasn't already been captured on the last visit
+        if dateTime > last_visited:
+            thumb = video['snippet']['thumbnails']['medium']
+            img = thumb['url']
+            id = video['snippet']['resourceId']['videoId']
+            link = "https://www.youtube.com/watch?v=" + video['snippet']['resourceId']['videoId']
+            videos_of_this_channel.append([dateTime, id, link, img])
+    return videos_of_this_channel
+
+#creating dictionaries for each vid and storing them in lists by month, with the month as the key of the parent dictionary
+def videos_by_month(all_videos):
+    start = time.perf_counter()
+    current_year_month = all_videos[0][0][0:7]
+    final_videos = {}
+    current_month = []
+    for video in all_videos:
+        video_details = {}
+        video_details["datetime"] = video[0]
+        video_details["id"] = video[1]
+        video_details["link"] = video[2]
+        video_details["img"] = video[3]
+
+        if video_details["datetime"] > current_year_month:
+            current_month.append(video_details)
+        else:
+            final_videos[current_year_month]=current_month
+            current_year_month = video_details["datetime"][0:7]
+            current_month = []
+            current_month.append(video_details)
+
+    final_videos[current_year_month]=current_month
+    return final_videos
+
+def year_month_index(all_videos):
+    month_index = {}
+    index = 0
+    for key in all_videos:
+        month_index[index] = key
+        index += 1
+    return month_index
+
+def index_year_month(all_videos):
+    month_index = {}
+    index = 0
+    for key in all_videos:
+        month_index[key] = index
+        index += 1
+    return month_index
+
+####################################################
+########### HANDLING REQUESTS #############
+####################################################
 
 
 @bp.route("/del-vid", methods=["POST"])
@@ -153,6 +180,7 @@ def delete_vid():
     # res = make_response(jsonify({"message":"JSON received"}),200)
     return video_id
 
+
 @bp.route("/undel-vid", methods=["POST"])
 def undelete_vid():
     req = request.get_json()
@@ -164,6 +192,79 @@ def undelete_vid():
         "WHERE user_id = ? AND video_id = ?", (current_user.id, video_id)
     )
     db.commit()
-
-    # res = make_response(jsonify({"message":"JSON received"}),200)
     return video_id
+
+
+@bp.route("/obtain-video-data", methods=["POST", "GET"])
+def send_data():
+    req = request.get_json()
+
+    # Get credentials from the current session
+    credentials = google.oauth2.credentials.Credentials(**session['credentials'])
+
+    # Use credentials to build an API client
+    youtube = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+    # New videos to populate page with
+    new_videos = collect_videos(youtube)
+
+    db = get_db()
+
+    old_videos = db.execute(
+        "SELECT date_time, video_id, video_url, video_img FROM video "
+        "WHERE user_id = ? AND deleted = 0 "
+        "ORDER BY date_time DESC", (current_user.id,)
+    ).fetchall()
+
+    for video in new_videos:
+        db.execute(
+            "INSERT INTO video (user_id, video_id, video_url, video_img, date_time, deleted) "
+            "VALUES (?, ?, ?, ?, ?, 0)",
+            (current_user.id, video[1], video[2], video[3], video[0])
+        )
+    db.commit()
+    # After videos are collected, update the last visited time of the user
+    User.update_time(current_user.id)
+    new_user = False
+    if current_user.last_visited == "0000-00-00T00:00:00Z":
+        new_user = True
+    all_videos = videos_by_month(new_videos + old_videos)
+    month_by_index = year_month_index(all_videos)
+    index_by_month = index_year_month(all_videos)
+    print(all_videos)
+    res = {
+        "new_user":new_user,
+        "monthly_data": all_videos,
+        "month_by_index" : month_by_index,
+        "index_by_month" : index_by_month,
+        "user_datetime": current_user.last_visited,
+    }
+    res = make_response(jsonify(res),200)
+    return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
