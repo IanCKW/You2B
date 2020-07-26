9e87bd07f2b688e1d374f0426c4dfeca399b0d06
Ian Chan <55784952+IanCKW@users.noreply.github.com>
2020-07-26 11:38:12 +0800

    quota

--- you2b/auth.py
+++ you2b/auth.py
@@ -29,10 +29,10 @@ from .user import User
 
 # Configuration
 TOTAL_SECRET_FILES = 1 # edit to number of json files available
-SECRET_FILE_NUM = randint(0, TOTAL_SECRET_FILES - 1)
-SECRET_FILE_NAME = 'client_secret_file_' + str(SECRET_FILE_NUM) + '.json'
+SECRET_FILE_NUM = randint(1, TOTAL_SECRET_FILES)
+SECRET_FILE_NAME = 'csf' + str(SECRET_FILE_NUM) + '.json'
 
-CLIENT_SECRETS_FILE = Path(__file__).parent + SECRET_FILE_NAME # changes from the whole thing
+CLIENT_SECRETS_FILE = os.path.join(os.path.dirname(__file__), SECRET_FILE_NAME) # changes from the whole thing
 SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
